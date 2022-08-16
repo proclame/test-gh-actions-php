8f8368d6c4708c9a748434a16e5a9481e270a493
Nick Mispoulier <nick@proclame.be>
2022-08-16 22:44:55 +0000

    set no_extra_blank_lines rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,6 +9,7 @@ return $config->setRules([
         '@PSR12' => true,
         'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'no_extra_blank_lines' => true,
     ])
     ->setFinder($finder)
 ;
