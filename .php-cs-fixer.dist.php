e8049d4e5c4961ddd0c7cc98d44fc7d47f917908
Nick Mispoulier <nick@proclame.be>
2022-08-16 22:54:32 +0000

    Set rule: single_blank_line_at_eof

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,6 +7,7 @@ $finder = PhpCsFixer\Finder::create()
 $config = new PhpCsFixer\Config();
 return $config->setRules([
         'no_closing_tag' => true,
+        'single_blank_line_at_eof' => true,
         'no_extra_blank_lines' => true,
     ])
     ->setFinder($finder)
